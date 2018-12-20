02d6feec0d19f1b73c5e669418253d213b076e1d
KailongLi <kailong.sjtu@gmail.com>
2018-12-20 19:53:07 +0800

    complete elb-client-setup.sh

--- elb-client-setup.sh
+++ elb-client-setup.sh
@@ -2,15 +2,22 @@
 mode=$1
 
 yum install python2 vim -y
-lb_ip="114.116.108.69"
+lb_ip="192.168.1.17"
 if [ -z $mode ]; then
 echo "Usage: `basaname $0` [scon|lcon|sudp|ludp]"
 exit 1
 fi
 
-if [ $mode -eq "scon" ]; then
-
-elseif 
+if [ $mode == "scon" ]; then
+sh curl.sh $lb_ip 80
+elif [ $mode == "lcon" ]; then
+nohup python sock_connect.py $lb_ip 8080 &
+sleep 2
+tailf $lb_ip\:8080.log
+elif [ $mode == "sudp" ]; then
 python udp_client.py 1000 $lb_ip  5001 
+elif [ $mode == "ludp" ]; then
+python udp_client.py 2000 $lb_ip  5001 
+fi
 
 
---
+++ git-config.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git config --global user.email "kailong.sjtu@gmail.com"
+git config --global user.name "KailongLi"
